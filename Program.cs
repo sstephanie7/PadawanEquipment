fd80d57cb4203d80e24f8330601286473a15b579
sstephanie7 <106015292+sstephanie7@users.noreply.github.com>
2022-05-24 18:00:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amountOfMoney = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceOfLightsabers = double.Parse(Console.ReadLine());
+            double priceOfRobes = double.Parse(Console.ReadLine());
+            double priceOfBelts = double.Parse(Console.ReadLine()); 
+
+            double sabersTotal = Math.Ceiling(students * 1.10);
+            double totalForLightsabers = priceOfLightsabers * sabersTotal;
+            double totalForRobes = priceOfRobes * students;
+
+            double freeBelts = Math.Floor(students / 6.0);
+            double totalForBelts = (students - freeBelts) * priceOfBelts;
+
+            double totalForEquipment = totalForLightsabers + totalForRobes + totalForBelts;
+
+            if (totalForEquipment <= amountOfMoney)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalForEquipment:f2}lv."); 
+            }
+            else
+            {
+                double neededMoney = totalForEquipment - amountOfMoney;
+                Console.WriteLine($"John will need {neededMoney:f2}lv more.");
+            }
+
+
+        }   
+    }
+}
